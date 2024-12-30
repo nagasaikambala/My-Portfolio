d30ed36daa5c7e6f0768e7db54ae3892b0839e1e
nagasaikambala <113850235+nagasaikambala@users.noreply.github.com>
2024-12-30 22:20:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add interactivity if needed
+document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thank you for your message! I will get back to you soon.');
+});
